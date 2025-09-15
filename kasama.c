b04c950dad25da67febf026816a5722128b0cab8
Alexander E Tomack <alexander.e.tomack@vanderbilt.edu>
2025-09-15 08:49:19 -0500

    setting slave/master, move semantics

--- kasama.c
+++ kasama.c
@@ -42,13 +42,57 @@ struct X11 {
     int buf_x, buf_y;
 };
 
-bool
-term_set_size(struct PTY *pty, struct X11 *x11) {
+bool term_set_size(struct PTY *pty, struct X11 *x11) {
     struct winsize ws = {
         .ws_col = x11->buf_w,
         .ws_row = x11->buf_h,
     };
 
+    // On success, 0 is returned. On error, -1.
+    if (ioctl(pty->master,  // open file descriptor for terminal
+        TIOCSWINSZ,         // Device-dependent operation code to set winsize
+        &ws)                // Pointer to address of ref to winsize
+        == -1) {    
+            perror("ioctl(TIOCSWINS)");
+            return false;
+    }
+
     return true;
 }
+
+bool pt_pair(struct PTY *pty) {
+    char *slave_name;
+    
+    pty->master = posix_openpt(O_RDWR | O_NOCTTY);
+    if (pty->master == -1) {
+        perror("posix_openpt");
+        return false;
+    }
+
+    if (grantpt(pty->master) == -1) {
+        perror("grantpt");
+        return false;
+    }
+
+    if (unlockpt(pty->master) == -1) {
+        perror("grantpt");
+        return false;
+    }
+
+    slave_name = ptsname(pty->master);
+    if (slave_name == NULL) {
+        perror("ptsname");
+        return false;
+    }
+
+    pty->slave = open(slave_name, O_RDWR | O_NOCTTY);
+    if (pty->slave == -1) {
+        perror("open(slave_name)");
+        return false;
+    }
+
+    return true;
+}
+
+
 
